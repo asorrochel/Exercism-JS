70ce651bb05a56b72132aade141370567000a5dc
Adrian Sorroche <asorrocheL@protonmail.com>
2023-02-16 10:40:27 +0100

    Ej 21 - elyses-transformative-enchantments

---
+++ elyses-transformative-enchantments/enchantments.js
@@ -0,0 +1,96 @@
+// @ts-check
+
+/**
+ * Double every card in the deck.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} deck with every card doubled
+ */
+export function seeingDouble(deck) {
+  return deck.map((card) => card * 2)
+}
+
+/**
+ *  Creates triplicates of every 3 found in the deck.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} deck with triplicate 3s
+ */
+export function threeOfEachThree(deck) {
+  // deck.map(card => card === 3 ? [card,card,card]: card);
+  // return deck;
+
+  return deck.reduce((result, card) => {
+    if (card === 3) {
+      result.push(card, card, card)
+    } else {
+      result.push(card)
+    }
+    return result
+  }, [])
+}
+
+
+/**
+ * Extracts the middle two cards from a deck.
+ * Assumes a deck is always 10 cards.
+ *
+ * @param {number[]} deck of 10 cards
+ *
+ * @returns {number[]} deck with only two middle cards
+ */
+export function middleTwo(deck) {
+  let middleCards = deck.slice(4,6);
+  return middleCards;
+}
+
+/**
+ * Moves the outside two cards to the middle.
+ *
+ * @param {number[]} deck with even number of cards
+ *
+ * @returns {number[]} transformed deck
+ */
+
+export function sandwichTrick(deck) {
+
+  deck.splice(deck.length/2 -1,0, deck.pop(),deck.shift());
+  return deck;
+}
+
+/**
+ * Removes every card from the deck except 2s.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} deck with only 2s
+ */
+export function twoIsSpecial(deck) {
+  let deck2s = deck.filter((card) => card === 2);
+  return deck2s;
+}
+
+/**
+ * Returns a perfectly order deck from lowest to highest.
+ *
+ * @param {number[]} deck shuffled deck
+ *
+ * @returns {number[]} ordered deck
+ */
+export function perfectlyOrdered(deck) {
+  deck.sort((a, b) => a - b);
+  return deck;
+}
+
+/**
+ * Reorders the deck so that the top card ends up at the bottom.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} reordered deck
+ */
+export function reorder(deck) {
+  return deck.reverse();
+}
